c5ff20a6dc48876752c9ca384ecc49d3ffc62308
MuradAles <muradaleskerov99@gmail.com>
2023-05-20 19:52:25 -0400

    firebase fix

--- frontend/src/firebase/Firebase.js
+++ frontend/src/firebase/Firebase.js
@@ -4,11 +4,10 @@ import 'firebase/compat/auth';
 const firebaseApp = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 export default firebaseApp;
